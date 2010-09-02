9b770edfc39e53cb43249c880f2f04eeba769263
Matthew Allen <fret@memecode.com>
2010-09-02 00:25:12 +0000

    Win32 build fix

--- src/win32/Lgi/GClipBoard.cpp
+++ src/win32/Lgi/GClipBoard.cpp
@@ -303,7 +303,7 @@ GSurface *GClipBoard::Bitmap()
 	return pDC;
 }
 
-bool GClipBoard::Binary(int Format, uchar *Ptr, int Len, bool AutoEmpty)
+bool GClipBoard::Binary(FormatType Format, uchar *Ptr, int Len, bool AutoEmpty)
 {
 	bool Status = FALSE;
 
@@ -334,7 +334,7 @@ bool GClipBoard::Binary(int Format, uchar *Ptr, int Len, bool AutoEmpty)
 	return Status;
 }
 
-bool GClipBoard::Binary(int Format, uchar **Ptr, int *Len)
+bool GClipBoard::Binary(FormatType Format, uchar **Ptr, int *Len)
 {
 	bool Status = false;
 
