e188906670f8de686c763350881aac2f6b79d743
Matthew Allen <fret@memecode.com>
2011-01-10 00:34:09 +0000

    Win32 build fix.

--- src/common/Coding/GScripting.cpp
+++ src/common/Coding/GScripting.cpp
@@ -2444,8 +2444,10 @@ bool GExternFunc::Call(GScriptContext *Ctx, GVariant *Ret, ArgumentArray &In)
 			void *c = Lib.GetAddress(Method);
 			if (c)
 			{
-				// uint32 a = ArgVal.Length();
-				// uint32 b = (uint32) &ArgVal[Args.Length()-1];
+				#if defined(_MSC_VER)
+				uint32 a = ArgVal.Length();
+				uint32 b = (uint32) &ArgVal[Args.Length()-1];
+				#endif
 				uint32 r = 0;
 
 				#if defined(_MSC_VER)
