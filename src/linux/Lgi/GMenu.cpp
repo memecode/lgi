999cf37f14bb2160fd3c0ec8b621a5fb4a3e8129
Matthew Allen <fret@memecode.com>
2011-09-21 12:47:24 +0000

    Build fix.

--- src/linux/Lgi/GMenu.cpp
+++ src/linux/Lgi/GMenu.cpp
@@ -45,7 +45,7 @@ GMenuItem *GSubMenu::ItemAt(int Id)
 	return Items.ItemAt(Id);
 }
 
-GMenuItem *GSubMenu::AppendItem(const char *Str, int Id, bool Enabled, int Where, char *Shortcut)
+GMenuItem *GSubMenu::AppendItem(const char *Str, int Id, bool Enabled, int Where, const char *Shortcut)
 {
 	GMenuItem *i = new GMenuItem;
 	if (i)
@@ -288,7 +288,7 @@ GMenuItem::GMenuItem()
 	_Check = false;
 }
 
-GMenuItem::GMenuItem(GMenu *m, GSubMenu *p, int Pos, char *Shortcut)
+GMenuItem::GMenuItem(GMenu *m, GSubMenu *p, int Pos, const char *Shortcut)
 {
 	Info = 0;
 	Child = 0;
