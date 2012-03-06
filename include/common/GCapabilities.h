816d5069e91d3e0fa175e3c756d3305c50accfe2
Matthew Allen <fret@memecode.com>
2012-03-06 12:31:36 +0000

    Comments

--- include/common/GCapabilities.h
+++ include/common/GCapabilities.h
@@ -1,18 +1,26 @@
+/// \file
+/// \author Matthew Allen <fret@memecode.com>
 #ifndef _GCAPABILITIES_H
 #define _GCAPABILITIES_H
 #ifdef __cplusplus
 
 class GCapabilityTarget;
+
+/// This class is a parent for objects that need external dependencies 
+/// to be installed by an external source.
 class LgiClass GCapabilityClient
 {
     friend class GCapabilityTarget;
     GArray<GCapabilityTarget*> Targets;
 
 protected:
+	/// Call this when you need to fulfill an external dependency.
     bool NeedsCapability(char *Name);
 
 public:
     virtual ~GCapabilityClient();
+    
+    /// Call this to register a target that can install dependencies.
     void Register(GCapabilityTarget *t);
 };
 
@@ -22,7 +30,9 @@ class LgiClass GCapabilityTarget
     GArray<GCapabilityClient*> Clients;
 
 public:
-    virtual ~GCapabilityTarget();    
+    virtual ~GCapabilityTarget();
+    
+    /// This is called to install a dependency.
     virtual bool NeedsCapability(char *Name) = 0;
 };
 
