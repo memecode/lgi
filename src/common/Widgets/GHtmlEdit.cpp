988c282a65d69c2cf5db5ddc21f0b2c420300c28
Matthew Allen <fret@memecode.com>
2015-02-08 22:51:00 +0000

    Html edit fixes

--- src/common/Widgets/GHtmlEdit.cpp
+++ src/common/Widgets/GHtmlEdit.cpp
@@ -377,87 +377,88 @@ class HtmlEdit : public Html1::GHtml, public GDefaultDocumentEnv
 		return Px>0 && b.Y()>0 ? (float)Px/b.Y() : 0.0f;
 	}
 
+	#if 0
 	bool FindTextRect(GTag *t, Block::Direction Dir, Block &Blk, GdcPt2 BlkOff, int x, int y)
 	{
+		// Scan any text rects in this tag
+		for (unsigned n=0; n<t->TextPos.Length(); n++)
+		{
+			GFlowRect *r = t->TextPos[n];
+			if (!r) continue;
+
+			GRect pos = *r;
+			pos.Offset(BlkOff.x, BlkOff.y);
+			switch (Dir)
+			{
+				case Block::Left:
+				{
+					if (pos.OverlapY(y) && pos.x2 <= x)
+					{
+						default:
+							return false;
+						case Block::Left:
+						{
+							Blk.t = t;
+							Blk.fr = r;
+						}
+					}
+					break;
+				}
+				case Block::Right:
+				{
+					if (r->OverlapY(y) && r->x1 >= x)
+					{
+						// Left of current position... but is it nearest?
+						if (!Blk.fr || Blk.fr->x1 > r->x1)
+						{
+							Blk.t = t;
+							Blk.fr = r;
+						}
+					}
+					break;
+				}
+				case Block::Up:
+				{
+					if (r->y2 <= y)
+					{
+						if (!Blk.fr ||
+							r->y2 > Blk.fr->y2 ||
+							(r->OverlapY(Blk.fr) && r->x1 > Blk.fr->x1))
+						{
+							Blk.t = t;
+							Blk.fr = r;
+						}
+					}
+					break;
+				}
+				case Block::Down:
+				{
+					if (r->y1 >= y)
+					{
+						if (!Blk.fr ||
+							r->y1 < Blk.fr->y1 ||
+							(r->OverlapY(Blk.fr) && r->x2 < Blk.fr->x2))
+						{
+							Blk.t = t;
+							Blk.fr = r;
+						}
+					}
+					break;
+				}
+			}
+		}
+
 		// Scan through all the children looking for suitable text rects...
 		for (unsigned i=0; i<t->Children.Length(); i++)
 		{
 			GTag *c = ToTag(t->Children[i]);
 			if (!c) continue;
 			
+			GdcPt2 Off = BlkOff;
 			if (c->Display() == GCss::DispBlock)
-			{
-				GdcPt2 Off = BlkOff + c->Pos;
-				FindTextRect(c, Dir, Blk, Off, x, y);
-			}
-			else
-			{			
-				for (unsigned n=0; n<c->TextPos.Length(); n++)
-				{
-					GFlowRect *r = c->TextPos[n];
-					if (!r) continue;
+				Off = c->Pos;
 
-					switch (Dir)
-					{
-						default:
-							return false;
-						case Block::Left:
-						{
-							if (r->OverlapY(y) && r->x2 <= x)
-							{
-								// Left of current position... but is it nearest?
-								if (!Blk.fr || Blk.fr->x2 < r->x2)
-								{
-									Blk.t = c;
-									Blk.fr = r;
-								}
-							}
-							break;
-						}
-						case Block::Right:
-						{
-							if (r->OverlapY(y) && r->x1 >= x)
-							{
-								// Left of current position... but is it nearest?
-								if (!Blk.fr || Blk.fr->x1 > r->x1)
-								{
-									Blk.t = c;
-									Blk.fr = r;
-								}
-							}
-							break;
-						}
-						case Block::Up:
-						{
-							if (r->y2 <= y)
-							{
-								if (!Blk.fr ||
-									r->y2 > Blk.fr->y2 ||
-									(r->OverlapY(Blk.fr) && r->x1 > Blk.fr->x1))
-								{
-									Blk.t = c;
-									Blk.fr = r;
-								}
-							}
-							break;
-						}
-						case Block::Down:
-						{
-							if (r->y1 >= y)
-							{
-								if (!Blk.fr ||
-									r->y1 < Blk.fr->y1 ||
-									(r->OverlapY(Blk.fr) && r->x2 < Blk.fr->x2))
-								{
-									Blk.t = c;
-									Blk.fr = r;
-								}
-							}
-							break;
-						}
-					}
-				}
-			}
+			FindTextRect(c, Dir, Blk, Off, x, y, Depth + 1);
 		}
 		
 		return true;
@@ -486,6 +487,7 @@ class HtmlEdit : public Html1::GHtml, public GDefaultDocumentEnv
 		
 		return Ret;
 	}
+	#endif
 
 public:
 	GAutoWString OriginalSrcW;
