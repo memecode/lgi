20645ebaf656b5133f00acf467a67e4526d87df7
Matthew Allen <fret@memecode.com>
2015-06-23 12:51:12 +1000

    Fixed wrong dealloc in 8Bit app, NULL prog dlg ptr, removed logging

--- src/common/Widgets/GTableLayout.cpp
+++ src/common/Widgets/GTableLayout.cpp
@@ -33,7 +33,7 @@ enum CellFlag
 
 #define Izza(c)				dynamic_cast<c*>(v)
 // #define DEBUG_LAYOUT		535 // define to ID of control to dump (0 to disable)
-#define DEBUG_LAYOUT		105
+// #define DEBUG_LAYOUT		105
 #define DEBUG_PROFILE		0
 #define DEBUG_DRAW_CELLS	0
 
@@ -1420,7 +1420,6 @@ void GTableLayoutPrivate::Layout(GRect &Client)
 	int64 Start = LgiCurrentTime();
 	#endif
 
-	printf("Layout=%s\n", Client.GetStr());
 	LayoutHorizontal(Client);
 	LayoutVertical(Client);
 	LayoutPost(Client);
