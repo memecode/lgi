d574db192386c4e287c83c82e6ec09d97142d41a
Matthew Allen <fret@memecode.com>
2016-02-22 19:09:55 +0000

    Adding show file stuff for Haiku

---
+++ src/beos/General/ShowFileProp_Beos.cpp
@@ -0,0 +1,17 @@
+#include "Lgi.h"
+
+void LgiShowFileProperties(OsView Parent, const char *Filename)
+{
+	LgiAssert(!"Impl me.");
+}
+
+bool LgiBrowseToFile(const char *Filename)
+{
+	/*
+	char Args[MAX_PATH];
+	sprintf(Args, "/e,/select,\"%s\"", Filename);
+	return LgiExecute("explorer", Args);
+	*/
+	LgiAssert(!"Impl me.");
+	return false;
+}
