e8cbe8fe69ba567341e531b7d05421768de6834c
Matthew Allen <fret@memecode.com>
2017-11-20 18:52:07 +1100

    Fixed missing glyph stuff.

--- src/common/Gdc2/Font/GFont.cpp
+++ src/common/Gdc2/Font/GFont.cpp
@@ -429,7 +429,7 @@ class GFontPrivate
 {
 public:
 	#ifdef WINDOWS
-	static GLibrary Gdi32;
+	static GAutoPtr<GLibrary> Gdi32;
 	#endif
 
 	// Data
@@ -492,7 +492,7 @@ public:
 };
 
 #ifdef WINDOWS
-GLibrary GFontPrivate::Gdi32("Gdi32");
+GAutoPtr<GLibrary> GFontPrivate::Gdi32;
 #endif
 
 GFont::GFont(const char *face, int point)
@@ -1154,28 +1154,33 @@ bool GFont::Create(const char *face, int height, GSurface *pSurface)
 			{
 				typedef DWORD (WINAPI *Proc_GetFontUnicodeRanges)(HDC, LPGLYPHSET);
 
-				Proc_GetFontUnicodeRanges GetFontUnicodeRanges = (Proc_GetFontUnicodeRanges)d->Gdi32.GetAddress("GetFontUnicodeRanges");
-				if (GetFontUnicodeRanges)
+				if (!d->Gdi32)
+					d->Gdi32.Reset(new GLibrary("Gdi32"));
+				if (d->Gdi32)
 				{
-					DWORD BufSize = GetFontUnicodeRanges(hDC, 0);
-					LPGLYPHSET Set = (LPGLYPHSET) new char[BufSize];
-					if (Set && GetFontUnicodeRanges(hDC, Set) > 0)
+					Proc_GetFontUnicodeRanges GetFontUnicodeRanges = (Proc_GetFontUnicodeRanges)d->Gdi32->GetAddress("GetFontUnicodeRanges");
+					if (GetFontUnicodeRanges)
 					{
-						for (DWORD i=0; i<Set->cRanges; i++)
+						DWORD BufSize = GetFontUnicodeRanges(hDC, 0);
+						LPGLYPHSET Set = (LPGLYPHSET) new char[BufSize];
+						if (Set && GetFontUnicodeRanges(hDC, Set) > 0)
 						{
-							WCRANGE *Range = Set->ranges + i;
-							for (int n=0; n<Range->cGlyphs; n++)
+							for (DWORD i=0; i<Set->cRanges; i++)
 							{
-								DWORD u = Range->wcLow + n;
-								if (u >> 3 < Bytes)
+								WCRANGE *Range = Set->ranges + i;
+								for (int n=0; n<Range->cGlyphs; n++)
 								{
-									d->GlyphMap[u>>3] |= 1 << (u & 7);
+									DWORD u = Range->wcLow + n;
+									if (u >> 3 < Bytes)
+									{
+										d->GlyphMap[u>>3] |= 1 << (u & 7);
+									}
 								}
 							}
 						}
-					}
 
-					DeleteArray((char*&)Set);
+						DeleteArray((char*&)Set);
+					}
 				}
 				
 				if (GTypeFace::d->IsSymbol)
