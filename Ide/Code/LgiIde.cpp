e291653c9ffcac19510949cc16c62a3196a5c76b
Matthew Allen <fret@memecode.com>
2018-07-17 18:09:25 +1000

    Linux fixes

--- Ide/Code/LgiIde.cpp
+++ Ide/Code/LgiIde.cpp
@@ -910,14 +910,14 @@ public:
 			{
 				char *Utf = &Buf[Channel][0];
 				#ifdef _DEBUG
-				/*
+				
 				printf("Utf: %p %i\n", Utf, (int)Size);
 				for (int i=0; i<Size; i++)
 				{
 					printf("%02x ", (uint8)Utf[i]);
 				}
 				printf("\n");
-				*/
+				
 				
 				if (!LgiIsUtf8(Utf, (ssize_t)Size))
 				{
