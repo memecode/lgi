e4e5ca3316b1a98143af58d4a3cf98a97ee42cf0
Matthew Allen <fret@memecode.com>
2019-10-29 11:22:39 +1100

    Javascript parser impl.

--- Ide/Code/JavascriptParser.cpp
+++ Ide/Code/JavascriptParser.cpp
@@ -12,54 +12,171 @@ Known bugs:
 #define DEBUG_LINE		550
 #endif
 
+#define IsWhiteSpace(c) \
+	(strchr(WhiteSpace, c) != NULL)
+#define IsBracket(c) \
+	((c) == '{' || (c) == '}')
+#define SkipWs(c) \
+	while (*c && strchr(WhiteSpace, *c)) \
+	{ \
+		if (*c == '\n') \
+			Line++; \
+		c++; \
+	}
+#define SkipSymbol(c) \
+	while (*c && (IsAlpha(*c) || IsDigit(*c) || strchr("_",*c))) \
+		c++;
+
 bool BuildJsDefnList(char *FileName, char16 *Source, GArray<DefnInfo> &Defns, int LimitTo, bool Debug)
 {
 	if (!Source)
 		return false;
 
-	GString Src = Source;
-	GString::Array Lines = Src.SplitDelimit("\n", -1, false);
 	int Depth = 0;
 	int Line = 1;
-	GString ClsName;
+	GString CurClass;
 
-	for (auto Ln : Lines)
+	for (auto *c = Source; *c; c++)
 	{
-		if (Depth)
+		switch (*c)
 		{
-			const char *s = Ln, *e = s;
-			while (iswhite(*e))
-				e++;
-			if ((e - s) < Depth && *e && *e != '#')
-			{
-				Depth = 0;
-			}
-		}
+			case '/':
+				if (c[1] == '/')
+				{
+					// Single line comment
+					c++;
+					while (c[1] && c[1] != '\n')
+						c++;
+				}
+				else if (c[1] == '*')
+				{
+					// Multi-line comment..
+					c += 2;
+					while (*c)
+					{
+						if (*c == '\n')
+							Line++;
+						if (c[0] == '*' && c[1] == '/')
+						{
+							c++;
+							break;
+						}
+						c++;
+					}
+				}
+				break;
+			case '\'':
+			case '\"':
+			case '`':
+				// Skip over string literal
+				for (char delim = *c++; *c; c++)
+				{
+					if (*c == '\n')
+						Line++;
+					if (*c == '\\')
+						c++;
+					else if (*c == delim)
+						break;
+				}				
+				break;
+			case '{':
+				if (Depth == 1 && CurClass)
+				{
+					auto *Start = c - 1;
+					int LocalLine = Line;
+					while (Start > Source && !IsBracket(*Start))
+					{
+						if (*Start == '\n') LocalLine--;
+						Start--;
+					}
+					if (IsBracket(*Start))
+					{
+						Start++;
 
-		if (Ln.Find("function ") == Depth)
-		{
-			if (ClsName)
-			{
-				GString::Array a = Ln.Strip().SplitDelimit(" \t", 1);
-				LgiAssert(a.Length() == 2);
-				GString Fn = a[0] + " " + ClsName + "::" + a[1];
-				Defns.New().Set(DefnFunc, FileName, Fn, Line);
-			}
-			else
-			{
-				Defns.New().Set(DefnFunc, FileName, Ln, Line);
-			}
+						while (*Start && strchr(WhiteSpace, *Start))
+						{
+							if (*Start == '\n') LocalLine++;
+							Start++;
+						}
+
+						GString Fn(Start, c - Start);
+
+						auto &d = Defns.New();
+						d.Type = DefnFunc;
+						d.Name = CurClass + "." + Fn.Strip();
+						d.File = FileName;
+						d.Line = LocalLine;
+					}
+				}
+				Depth++;
+				break;			
+			case '}':
+				Depth--;
+				if (Depth <= 0 && CurClass)
+					CurClass.Empty();
+				break;
+			case 'c':
+				if (!Strncmp(c, L"class", 5) &&
+					IsWhiteSpace(c[5]))
+				{
+					c += 5;
+					SkipWs(c);
+					auto *Start = c;
+					SkipSymbol(c);
+
+					auto &d = Defns.New();
+					d.Type = DefnClass;
+					CurClass = GString(Start, c - Start);
+					d.Name = CurClass;
+					d.File = FileName;
+					d.Line = Line;
+				}
+				break;
+			case 'f':
+				if (!Strncmp(c, L"function", 8) &&
+					IsWhiteSpace(c[8]))
+				{
+					c += 8;
+
+					auto &d = Defns.New();
+					d.Type = DefnFunc;
+
+					SkipWs(c);
+					auto *Start = c;
+					SkipSymbol(c);
+					d.FnName.Start = 0;
+					d.FnName.Len = c - Start;
+
+					int Bracket = 0;
+					while (*c)
+					{
+						if (*c == '\n')
+							Line++;
+						if (*c == '(') Bracket++;
+						else if (*c == ')')
+						{
+							if (--Bracket <= 0)
+							{
+								c++;
+								break;
+							}
+						}
+						c++;
+					}
+
+					GString FnDecl = GString(Start, c - Start);
+					d.Name = FnDecl;
+					d.File = FileName;
+					d.Line = Line;
+					c--;
+				}
+				break;
+			case '\n':
+				Line++;
+				break;
+			default:
+				break;
 		}
-		else if (Ln.Find("class ") == Depth)
-		{
-			GString::Array a = Ln.SplitDelimit(" \t(");
-			LgiAssert(a.Length() >= 2);
-			ClsName = a[1];
-			auto ClsDesc = a[0] + " " + a[1];
-			Defns.New().Set(DefnClass, FileName, ClsDesc, Line);
-			Depth = 4;
-		}
-		Line++;
 	}
 
 	return Defns.Length() > 0;
