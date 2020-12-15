6db9dab9d6c2c435871dab2273e4ced2af1445cc
Matthew Allen <fret@memecode.com>
2020-12-15 20:16:42 +1100

    Assert fix in html editor.

--- src/common/Text/GTextView3.cpp
+++ src/common/Text/GTextView3.cpp
@@ -1352,6 +1352,8 @@ void GTextView3::PourStyle(size_t Start, ssize_t EditSize)
 		return;
 
 	ssize_t Length = MAX(EditSize, 0);
+	if (Start + Length >= Size)
+		Length = Size - Start; // For deletes, this sizes the edit length within bounds.
 
 	// Expand re-style are to word boundaries before and after the area of change
 	while (Start > 0 && UrlChar(Text[Start-1]))
