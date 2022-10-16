308398f9bb5f21cdd70de80ce58d1f213bf2a4fe
Matthew Allen <fret@memecode.com>
2022-10-17 10:42:50 +1100

    Win: Use AlphaBlend for 24bit images too... quality is way better than stretchblt.

--- src/win/Gdc2/MemDC.cpp
+++ src/win/Gdc2/MemDC.cpp
@@ -664,8 +664,7 @@ void LMemDC::StretchBlt(LRect *Dest, LSurface *Src, LRect *s)
 			}
 			case GDC_ALPHA:
 			{
-				if (GdcD->AlphaBlend &&
-					Src->GetBits() == 32)
+				if (GdcD->AlphaBlend)
 				{
 					HDC hDestDC = StartDC();
 					HDC hSrcDC = Src->StartDC();
@@ -677,7 +676,7 @@ void LMemDC::StretchBlt(LRect *Dest, LSurface *Src, LRect *s)
 												d->ConstAlpha <= 255 ?
 												d->ConstAlpha :
 												255;
-					Blend.AlphaFormat = AC_SRC_ALPHA;
+					Blend.AlphaFormat = Src->GetBits() == 32 ? AC_SRC_ALPHA : 0;
 
 					if (!GdcD->AlphaBlend(	hDestDC,
 											DestR.x1, DestR.y1, DestR.X(), DestR.Y(),
