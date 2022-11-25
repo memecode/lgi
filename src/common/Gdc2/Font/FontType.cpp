0201e61d4b87a6044c040ee7da5db5dafc7a0d14
Matthew Allen <fret@memecode.com>
2022-11-26 09:59:37 +1100

    Mac build fixes.

--- src/common/Gdc2/Font/FontType.cpp
+++ src/common/Gdc2/Font/FontType.cpp
@@ -399,7 +399,7 @@ bool LFontType::GetSystemFont(const char *Which)
 	if (Offset)
 	{
 		auto i = Offset.Int();
-		if (i != 0 && std::abs(i < 20))
+		if (i != 0 && std::abs(i) < 20)
 			PtSizeOffset = (int)i;
 	}
 
