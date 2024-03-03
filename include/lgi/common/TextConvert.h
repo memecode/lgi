c2b39cee274d1d935c6d6410ffdb3ac8ac878b9a
Matthew Allen <fret@memecode.com>
2024-03-04 08:26:56 +1100

    Build fix.

--- include/lgi/common/TextConvert.h
+++ include/lgi/common/TextConvert.h
@@ -2,14 +2,14 @@
 
 bool Is8Bit(const char *Text);
 
-[[deprecated]] LgiFunc char *DecodeBase64Str(char *Str, ssize_t Len = -1);
-LgiExtern LString LDecodeBase64Str(LString Str);
+[[deprecated]] char *DecodeBase64Str(char *Str, ssize_t Len = -1);
+LString LDecodeBase64Str(LString Str);
 
-[[deprecated]] LgiFunc char *DecodeQuotedPrintableStr(char *Str, ssize_t Len = -1);
-LgiExtern LString LDecodeQuotedPrintableStr(LString Str);
+[[deprecated]] char *DecodeQuotedPrintableStr(char *Str, ssize_t Len = -1);
+LString LDecodeQuotedPrintableStr(LString Str);
 
-[[deprecated]] LgiFunc char *DecodeRfc2047(char *Str);
-LgiExtern LString LDecodeRfc2047(LString Str);
+[[deprecated]] char *DecodeRfc2047(char *Str);
+LString LDecodeRfc2047(LString Str);
 
-[[deprecated]] LgiFunc char *EncodeRfc2047(char *Input, const char *InCharset, LString::Array *OutCharsets = NULL, ssize_t LineLength = 0);
-LgiExtern LString LEncodeRfc2047(LString Input, const char *InCharset, LString::Array *OutCharsets = NULL, ssize_t LineLength = 0);
+[[deprecated]] char *EncodeRfc2047(char *Input, const char *InCharset, LString::Array *OutCharsets = NULL, ssize_t LineLength = 0);
+LString LEncodeRfc2047(LString Input, const char *InCharset, LString::Array *OutCharsets = NULL, ssize_t LineLength = 0);
