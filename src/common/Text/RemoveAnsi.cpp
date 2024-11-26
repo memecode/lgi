a19df19988ede066a2ad8881f35baac0ca9f115b
Matthew Allen <fret@memecode.com>
2024-11-27 08:28:30 +1100

    Fix the remove ansi function.

--- src/common/Text/RemoveAnsi.cpp
+++ src/common/Text/RemoveAnsi.cpp
@@ -13,45 +13,75 @@ void RemoveAnsi(LString &a)
 	a.Length(newSize);
 }
 
+#define FunctionChar(ch) (IsAlpha(ch) || (ch) == '~')
+
 size_t RemoveAnsi(char *in, size_t length)
 {
 	auto *s = in;
 	auto *out = in;
 	auto e = s + length;
+	bool echoChars = true;
 
 	while (s < e)
 	{
 		if (*s == 0x7)
+		{
 			s++; // skip
+		}
 		else if
 		(
 			*s == 0x1b
 			&&
 			s + 1 < e
-			&&
-			s[1] >= 0x40
-			&&
-			s[1] <= 0x5f
 		)
 		{
-			// ANSI seq
-			if (s[1] == '[' &&
-				s[2] == '0' &&
-				s[3] == ';')
+			if (s[1] == '[' || s[1] == 0x9B)
 			{
-				s += 4;
+				// Control Sequence Introducer
+				while (s < e && !FunctionChar(*s))
+					s++; // skip the param chars
+				if (s < e)
+					s++; // skip the command char
+			}
+			else if (s[1] == '7' || s[1] == '8')
+			{
+				// Save/restore cursor
+				s += 2;
+			}
+			else if
+			(
+				(s[1] == 'P' || s[1] == 0x90) // Device Control String
+				||
+				(s[1] == ']' || s[1] == 0x9D) // Operating System Command
+				||
+				(s[1] == 'X') // Start of String
+				||
+				(s[1] == '^') // Privacy message
+				||
+				(s[1] == '_') // App command
+			)
+			{
+				
+				echoChars = false;
+				s += 2;
+			}
+			else if (s[1] == '/')
+			{
+				echoChars = true;
+				s += 2;
 			}
 			else
 			{
-				s += 2;
-				while (s < e && !IsAlpha(*s))
-					s++;
-				if (s < e)
-					s++;
+				*out++ = *s++;
 			}
-
 		}
-		else *out++ = *s++;
+		else
+		{
+			if (echoChars)
+				*out++ = *s++;
+			else
+				s++;
+		}
 	}
 
 	return out - in;
