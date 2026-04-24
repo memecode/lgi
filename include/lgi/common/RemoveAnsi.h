b2fc6872147c354700e9bac98441c25b82eca020
Matthew Allen <fret@memecode.com>
2026-04-24 15:41:41 +1000

    Remove ansi stream impl.

--- include/lgi/common/RemoveAnsi.h
+++ include/lgi/common/RemoveAnsi.h
@@ -3,3 +3,19 @@
 extern size_t RemoveAnsi(char *s, size_t length);
 extern void RemoveAnsi(LArray<char> &a);
 extern void RemoveAnsi(LString &a);
+
+// If you need stateful parsing of ansi codes across memory blocks, use this:
+// It _should_ handle partial ansi escape codes between Write calls.
+class LgiClass LRemoveAnsiStream : public LStream
+{
+	LStream *out = nullptr;
+	struct AnsiParser *d;
+	
+public:
+	LRemoveAnsiStream(LStream *outputStream);	
+	~LRemoveAnsiStream();
+	
+	// Write data with ansi to this stream.. the non-ansi data is written to
+	// the 'outputStream' passed to the constructor.
+	ssize_t Write(const void *Ptr, ssize_t Size, int Flags = 0) override;
+};
--- src/common/Text/RemoveAnsi.cpp
+++ src/common/Text/RemoveAnsi.cpp
@@ -1,6 +1,159 @@
 #include "lgi/common/Lgi.h"
 #include "lgi/common/RemoveAnsi.h"
 
+#define FunctionChar(ch) (IsAlpha(ch) || (ch) == '~')
+
+struct AnsiParser
+{
+	constexpr static int cBELL   = 0x07;
+	constexpr static int cESCAPE = 0x1b;
+	
+	bool echoChars = true;
+	uint8_t ansi[256] = "";
+	int used = 0;
+	
+	enum TState {
+		TNon,
+		TEscape,
+		TCsi,
+		TCursor,
+		TString,
+	}	state = TNon;
+
+	void parse(	std::function<void(uint8_t*,size_t)> nonAnsi,
+				std::function<void(uint8_t*,size_t)> onAnsi,
+				const char *in,
+				size_t length)
+	{
+		auto s = (uint8_t*) in;
+		auto *out = in;
+		auto e = s + length;
+		uint8_t *startNon = nullptr;
+		int u = 0;
+		
+		auto emitNonAnsi = [&]()
+			{
+				if (startNon)
+				{
+					if (nonAnsi)
+						nonAnsi(startNon, s - startNon);
+					startNon = nullptr;
+				}
+			};
+		auto emitAnsi = [&]()
+			{
+				if (used > 0)
+				{
+					if (onAnsi)
+						onAnsi(ansi, used);
+					ansi[used = 0] = 0;
+				}
+			};
+		
+		while (s < e)
+		{
+			switch (state)
+			{
+				case TNon:
+				{
+					if (*s == cBELL)
+					{
+						emitNonAnsi();
+					}
+					else if (*s == 0x9c && echoChars == false) // I'm not sure if this is correct...
+					{
+						echoChars = true;
+					}
+					else if (*s == cESCAPE)
+					{
+						emitNonAnsi();
+						
+						// Start the escape sequence:
+						state = TEscape;
+						used = 0;
+						ansi[used++] = *s;
+					}
+					else if (!startNon && echoChars)
+					{
+						startNon = s;
+					}
+					break;
+				}
+				case TEscape:
+				{
+					ansi[used++] = *s;
+					
+					// What type of escape is it?
+					if (*s == '[' || *s == 0x9B)
+					{
+						state = TCsi; // Control Sequence Introducer
+					}
+					else if (*s == '7' || *s == '8')
+					{
+						// Save/restore cursor
+						state = TCursor;
+					}
+					else if
+					(
+						(*s == 'P' || *s == 0x90) // Device Control String
+						||
+						(*s == ']' || *s == 0x9D) // Operating System Command
+						||
+						(*s == 'X') // Start of String
+						||
+						(*s == '^') // Privacy message
+						||
+						(*s == '_') // App command
+					)
+					{				
+						echoChars = false;
+						state = TString;
+					}
+					else
+					{
+						// Not a valid ANSI?
+						state = TNon;
+					}
+					break;
+				}
+				case TCsi: // Control Sequence Introducer
+				{
+					if (FunctionChar(*s))
+					{
+						emitAnsi();
+						state = TNon;
+					}
+					else if (used < sizeof(ansi))
+					{
+						ansi[used++] = *s;
+					}
+					break;
+				}
+				case TCursor:
+				{
+					ansi[used++] = *s;
+					emitAnsi();
+					state = TNon;
+					break;
+				}
+				case TString:
+				{
+					if (*s == 0x9C)
+					{
+						emitAnsi();
+						state = TNon;
+					}
+					break;
+				}
+			}
+			
+			s++;
+		}
+		
+		emitNonAnsi();
+	}
+};
+
 void RemoveAnsi(LArray<char> &a)
 {
 	auto newSize = RemoveAnsi(a.AddressOf(), a.Length());
@@ -13,84 +166,59 @@ void RemoveAnsi(LString &a)
 	a.Length(newSize);
 }
 
-#define FunctionChar(ch) (IsAlpha(ch) || (ch) == '~')
-
 size_t RemoveAnsi(char *in, size_t length)
 {
-	auto s = (uint8_t*) in;
-	auto *out = in;
-	auto e = s + length;
-	bool echoChars = true;
+	if (!in)
+		return 0;
 
-	while (s < e)
-	{
-		if
-		(
-			*s == 0x7
-			||
-			(*s == 0x9c && echoChars == false) // I'm not sure if this is correct...
-		)
-		{
-			s++; // skip
-			echoChars = true;
-		}
-		else if
-		(
-			*s == 0x1b
-			&&
-			s + 1 < e
-		)
-		{
-			if (s[1] == '[' || s[1] == 0x9B)
-			{
-				s += 2;
-
-				// Control Sequence Introducer
-				while (s < e && !FunctionChar(*s))
-					s++; // skip the param chars
-				if (s < e)
-					s++; // skip the command char
-			}
-			else if (s[1] == '7' || s[1] == '8')
-			{
-				// Save/restore cursor
-				s += 2;
-			}
-			else if
-			(
-				(s[1] == 'P' || s[1] == 0x90) // Device Control String
-				||
-				(s[1] == ']' || s[1] == 0x9D) // Operating System Command
-				||
-				(s[1] == 'X') // Start of String
-				||
-				(s[1] == '^') // Privacy message
-				||
-				(s[1] == '_') // App command
-			)
-			{				
-				echoChars = false;
-				s += 2;
-			}
-			else if (s[1] == '/')
-			{
-				echoChars = true;
-				s += 2;
-			}
-			else
-			{
-				*out++ = *s++;
-			}
-		}
-		else
-		{
-			if (echoChars)
-				*out++ = *s++;
-			else
-				s++;
-		}
-	}
-
-	return out - in;
-}
+	AnsiParser p;
+	char *out = in;
 	
+	p.parse([&](
+		// non-ansi cb:
+		auto ptr, auto sz)
+		{
+			memcpy(out, ptr, sz);
+			out += sz;
+		},
+		// ansi cb:
+		nullptr,
+		in,
+		length);
+	
+	// null-terminate string
+	*out = 0;
+	
+	return out - in;
+}	
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+LRemoveAnsiStream::LRemoveAnsiStream(LStream *outputStream)
+{
+	out = outputStream;
+	d = new AnsiParser;
+}
+
+LRemoveAnsiStream::~LRemoveAnsiStream()
+{
+	delete d;
+}
+
+ssize_t LRemoveAnsiStream::LRemoveAnsiStream::Write(const void *Ptr, ssize_t Size, int Flags)
+{
+	ssize_t processed = 0;
+	
+	d->parse(
+		// non-ansi cb:
+		[&](auto ptr, auto sz)
+		{
+			if (out)
+				out->Write(ptr, sz);
+			processed += sz;
+		},
+		// ansi cb:
+		nullptr,
+		(char*)Ptr, Size);
+		
+	return processed;
+}
